081853423bb58f8282ca1bb4ec7a5d32ccb1e331
sangbose <sangeeta.nbose@gmail.com>
2019-10-03 20:01:41 +0530

    first commit

---
+++ src/jdataalgo/mypackage/MyMainClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jdataalgo.mypackage;
+
+/**
+ * @author sange
+ *
+ */
+public class MyMainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("My first program");
+	}
+
+}
